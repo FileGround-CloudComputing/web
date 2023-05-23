21554e8f45c05b351fa132a4bc92d1a90fb5855f
bluejoyq <bluejoy32@gmail.com>
2023-05-23 21:00:43 +0900

    feat: install firebase

--- src/client.d.ts
+++ src/client.d.ts
@@ -1,6 +1,13 @@
 interface ImportMetaEnv {
   readonly VITE_EXAMPLE: string;
-  // 다른 환경 변수들에 대한 타입 정의...
+  readonly VITE_FB_API_KEY: string;
+  readonly VITE_FB_DATABASE_URL: string;
+  readonly VITE_FB_AUTH_DOMAIN: string;
+  readonly VITE_FB_PROJECT_ID: string;
+  readonly VITE_FB_STORAGE_BUCKET: string;
+  readonly VITE_FB_MESSAGING_SENDER_ID: string;
+  readonly VITE_FB_APP_ID: string;
+  readonly VITE_FB_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
--- src/presentation/pages/MainPage.tsx
+++ src/presentation/pages/MainPage.tsx
@@ -6,23 +6,9 @@ import { css } from "@emotion/react";
 import { ImageFrame } from "../components/Frame";
 import { pageStyles } from "../styles/shape";
 import { logoStyles, typoStyles } from "../atomics/typo";
-import { FlowContainer } from "../components/Flow";
 export const MainPage = (): ReactElement => {
   return (
     <div css={pageStyles}>
-      <FlowContainer>
-        <h2
-          css={(theme) => css`
-            ${logoStyles}
-            font-size: 64px;
-            text-align: center;
-            color: ${theme.colors.onBackground};
-            padding: 0 16px;
-          `}
-        >
-          FRONTEND DEVELOPER
-        </h2>
-      </FlowContainer>
       <ImageFrame
         src={
           "https://images.unsplash.com/photo-1634129366530-61d3e56a84fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80"
