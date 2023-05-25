40f9d210b7c75e9e9f9c1fb9df1e15e1b96e9248
bluejoyq <bluejoy32@gmail.com>
2023-05-26 02:11:21 +0900

    refactor: main 페이지 이동

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { MainPage } from "./presentation/pages/MainPage";
+import { MainPage } from "./presentation/mainPage/MainPage";
 import { CONNECT_PATH, MAIN_PATH } from "./domain/paths";
 import { ConnectPage } from "./presentation/connectPage/ConnectPage";
 import { GroundPageEnter } from "./presentation/groundPage/GroundPage";
